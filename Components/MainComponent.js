4feb3f80b83b9f4218fdf6e0a36b190469b8f2fc
salman754321 <salman754321@gmail.com>
2020-07-18 19:43:19 +0500

    Implimented Navigation

---
+++ Components/MainComponent.js
@@ -0,0 +1,41 @@
+import React,{Component} from "react"
+import { View , Platform } from 'react-native';
+import Menu from "./MenuComponent"
+import DishDetail from "./DishDetails"
+
+import { createStackNavigator } from 'react-navigation';
+
+
+const MenuNavigator = createStackNavigator({
+    Menu: { screen: Menu },
+    DishDetail: { screen: DishDetail }
+},
+{
+    initialRouteName: 'Menu',
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: "#512DA8"
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: "#fff"            
+        }
+    }
+}
+);
+
+class Main extends Component{
+
+   
+  
+    render(){
+        return(
+
+           <View  style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+                <MenuNavigator />
+           </View>
+          
+        )
+    }
+}
+export default Main;
