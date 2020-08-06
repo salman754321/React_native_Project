4e61eda2e5565ee64bb99fb4c161df1a964882ee
salman754321 <salman754321@gmail.com>
2020-08-06 19:36:49 +0500

    Add Comments to DishDetailComponent

--- Components/DishDetails.js
+++ Components/DishDetails.js
@@ -1,7 +1,7 @@
 import React ,{Component} from "react"
-import {View,Text,ScrollView} from 'react-native'
-import {Card} from "react-native-elements"
-import { DISHES } from "../shared/dishes"
+import {View,Text,ScrollView,FlatList } from 'react-native'
+
+import { Card, Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 
@@ -13,38 +13,95 @@ const mapStateToProps = state => {
     
   }
 
-function RendeerDish(props){
-    const dish=props.dish;
-    console.log(dish);
-    if(dish!=null){
-return(
-        <Card
-            featuredTitle={dish.name}
-            image={{uri: baseUrl + dish.image}}
-        >
-            <Text style={{margin:10, fontSize:15,color:"#000"}}>
-                {dish.description}
-            </Text>
-        </Card>)
-    }else{
-        return (
-            <View></View>
-        )
-    }   
+  function RenderDish(props) {
 
+    const dish = props.dish;
+    
+        if (dish != null) {
+            return(
+                <Card
+                featuredTitle={dish.name}
+                image={{ uri: baseUrl + dish.image}}>
+                    <Text style={{margin: 10}}>
+                        {dish.description}
+                    </Text>
+                    <Icon
+                        raised
+                        reverse
+                        name = { props.favorite ? 'heart' : 'heart-o' }
+                        type = 'font-awesome'
+                        color = '#f50'
+                        onPress = {() => props.favorite ? console.log('Already favorite') : props.onPress()}
+                        />
+                </Card>
+            );
+        }
+        else {
+            return(<View></View>);
+        }
+}
+
+function RenderComments(props) {
+    const comments = props.comments;
+
+    const renderCommentItem = ({ item, index }) => {
+        return(
+            <View key = {index} style = {{margin: 10}}>
+                <Text style = {{fontSize: 14}}>
+                    {item.comment}
+                </Text>
+                <Text style = {{fontSize: 12}}>
+                    {item.rating} Stars
+                </Text>
+                <Text style = {{fontSize: 12}}>
+                    {'-- ' + item.author + ', ' + item.date}
+                </Text>
+            </View>
+        );
+    }
+
+    return(
+        <Card title = "Comments">
+            <FlatList
+                data = { comments }
+                renderItem = { renderCommentItem }
+                keyExtractor = { item => item.id.toString()}
+                />
+        </Card>
+    );
 }
 class DishDetail extends Component{
     
     static navigationOptions = {
         title: 'DishDetail'
     };
-    render(){
-        const dishId=this.props.navigation.getParam("dishid","");
-      console.table(this.props.dishes.dishes[+dishId]);
-    return(
-        <ScrollView>
-    <RendeerDish dish={this.props.dishes.dishes[+dishId]} />
-    </ScrollView>);
+    constructor(props) {
+        super(props);
+        this.state = {
+            favorites: []
+        };
+    }
+
+    markFavorite(dishId) {
+        this.setState({ favorites: this.state.favorites.concat(dishId) })
+    }
+
+    static navigationOptions = {
+        title: 'Dish Details'
+    };
+
+    render() {
+        const dishId = this.props.navigation.getParam('dishId','');
+        return(
+            <ScrollView>
+                <RenderDish dish={this.props.dishes.dishes[+dishId]}
+                    favorite = {this.state.favorites.some(el => el === dishId)}
+                    onPress = {() => this.markFavorite(dishId)}
+                    />
+                <RenderComments comments = {this.props.comments.comments.filter((comment) => comment.dishId === dishId)} />
+            </ScrollView>
+        );
     }
 }
+
 export default connect(mapStateToProps)(DishDetail);
