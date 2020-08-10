3b2105e0f4fd4869c3b1cb9d893b865dc720f52b
salman754321 <salman754321@gmail.com>
2020-08-10 23:37:26 +0500

    Gesture Part1

--- Components/DishDetails.js
+++ Components/DishDetails.js
@@ -1,5 +1,5 @@
 import React ,{Component} from "react"
-import {View,Text,ScrollView,FlatList, Modal,StyleSheet ,Button} from 'react-native'
+import {View,Text,ScrollView,FlatList, Modal,StyleSheet ,Button,Alert,PanResponder} from 'react-native'
 
 import { Card, Icon,Rating,Input  } from 'react-native-elements';
 import { connect } from 'react-redux';
@@ -21,14 +21,42 @@ const mapStateToProps = state => {
     postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
 })
 
+const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
+    if ( dx < -200 )
+        return true;
+    else
+        return false;
+}
+
+
 
   function RenderDish(props) {
 
     const dish = props.dish;
     
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: (e, gestureState) => {
+            return true;
+        },
+        onPanResponderEnd: (e, gestureState) => {
+            console.log("pan responder end", gestureState);
+            if (recognizeDrag(gestureState))
+                Alert.alert(
+                    'Add Favorite',
+                    'Are you sure you wish to add ' + dish.name + ' to favorite?',
+                    [
+                    {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                    {text: 'OK', onPress: () => {props.favorite ? console.log('Already favorite') : props.onPress()}},
+                    ],
+                    { cancelable: false }
+                );
+    
+            return true;
+        }
+    })
         if (dish != null) {
             return(
-                <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+                <Animatable.View animation="fadeInDown" duration={2000} delay={1000}  {...panResponder.panHandlers}>
                 <Card
                 featuredTitle={dish.name}
                 image={{ uri: baseUrl + dish.image}}>
